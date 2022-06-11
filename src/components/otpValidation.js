b41561d8724250614ae5b02d312266e90bf331ab
Miscellaneous Tasks <bhanumathurthi01@gmail.com>
2022-06-11 09:50:30 +0530

    otp page verification

--- src/components/otpValidation.js
+++ src/components/otpValidation.js
@@ -31,7 +31,7 @@ const OtpValidation = ({ phone }) => {
 
     axios
       .post(verifyotpurl, {
-        phone: `${phone}`,
+        phone: "phone",
         otp,
       })
       .then((response) => {
